00de43487cad2321a19d62cd4738c679fa09cb98
Ryan <sally4117678912qwas34@gmailcom>
2019-04-01 18:46:40 +0900

    calc avg len of data

--- re.py
+++ re.py
@@ -1,10 +1,16 @@
 data = []
 count = 0
+y = 0
+z = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[3])
+print('read finished, there are ', len(data), 'data')
+
+for x in data:
+	y += len(x)
+	z = y/len(data)
+print('the average length of the data is ', z)	
